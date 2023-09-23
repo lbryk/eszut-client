a1483e5596bb8e4a522a7cc29a85e11acf7211e4
Łukasz Bryk <wookashbryk@gmail.com>
2023-09-23 13:27:37 +0200

    add changes

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,3 +1,4 @@
+// confing file
 export const config = {
     backend: "https://eszut-server.zstz-radzymin.pl/",
     public_vapid: "BF0un_Oc5SD5LRAWZrx1ByWrBcwgshf-vN0vYQFOoFVDinTaadO1JqyYHmMZj6DSU7LLziNDm1mFXvZcvasxLWE"
